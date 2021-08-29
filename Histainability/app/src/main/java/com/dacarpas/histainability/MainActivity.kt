2cc3d0ebfa3ccc710038326b799e85e9cb160d6b
Anand <anandannu10@gmail.com>
2021-08-29 14:25:55 +0530

    Merge Conflict

--- Histainability/app/src/main/java/com/dacarpas/histainability/MainActivity.kt
+++ Histainability/app/src/main/java/com/dacarpas/histainability/MainActivity.kt
@@ -2,11 +2,8 @@ package com.dacarpas.histainability
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import okhttp3.*
-import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +20,7 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LinkActivity::class.java)
             startActivity(intent)
         }
-    }s
+    }
 
 }
 
