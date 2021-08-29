a8df600aa18a3981cca7f85e900757ca4a1128a2
pandaking <aditya9660sharma@gmail.com>
2021-08-29 11:55:23 +0530

    home page and quiz added

--- Histainability/app/src/main/java/com/dacarpas/histainability/MainActivity.kt
+++ Histainability/app/src/main/java/com/dacarpas/histainability/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.dacarpas.histainability
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var graph: TextView
+    private lateinit var settings : TextView
+    private lateinit var quiz : TextView
+    private lateinit var other_app : TextView
+    private lateinit var logo : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        graph = findViewById(R.id.graph_text)
+        settings = findViewById(R.id.settings_text)
+        quiz = findViewById(R.id.quiz_text)
+        other_app = findViewById(R.id.other_aap)
+        logo = findViewById(R.id.App_Logo)
+
+
+
+        graph.setOnClickListener {
+            val intent = Intent(this,GraphActivity::class.java)
+            startActivity(intent)
+        }
+
+        settings.setOnClickListener {
+            val intent = Intent(this,SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        quiz.setOnClickListener {
+            val intent = Intent(this,QuizActivity::class.java)
+            startActivity(intent)
+        }
+
+        other_app.setOnClickListener {
+            val intent = Intent(this,OtherAppsLinkActivity::class.java)
+            startActivity(intent)
+        }
+
+        logo.setOnClickListener {
+            Toast.makeText(this,"Made with ❤ by DACARPAS Group",Toast.LENGTH_LONG).show()
+        }
+
+
     }
 }
---
+++ Histainability/app/src/main/java/com/dacarpas/histainability/QuizModel.kt
@@ -0,0 +1,38 @@
+package com.dacarpas.histainability
+
+data class Question(var question: String,var options: List<String>)
+
+object Supplier {
+
+    val questions = listOf<Question>(
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+
+    )
+}
