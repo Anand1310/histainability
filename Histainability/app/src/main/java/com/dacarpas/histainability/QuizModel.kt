0a0f0fb64d12da473fcc41dd45abbbcd6f02665b
pandaking <aditya9660sharma@gmail.com>
2021-08-30 01:38:26 +0530

    Quiz section added

--- Histainability/app/src/main/java/com/dacarpas/histainability/QuizModel.kt
+++ Histainability/app/src/main/java/com/dacarpas/histainability/QuizModel.kt
@@ -1,38 +1,14 @@
 package com.dacarpas.histainability
 
-data class Question(var question: String,var options: List<String>)
+data class Question(var question: String,var options: List<String>, var correct_option : Int, var user_choice : Int)
 
 object Supplier {
 
     val questions = listOf<Question>(
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
-        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above")),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above"), 0,-1),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above"), 1,-1),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above"), 2,-1),
+        Question("What is life ?", listOf("Joke", "Pain", "Opposite of good","All of the above"), 3,-1),
 
     )
 }
