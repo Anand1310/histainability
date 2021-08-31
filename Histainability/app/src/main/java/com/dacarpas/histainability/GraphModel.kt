fc99e900cc3abba8915014c0ec0f0b62d3248a41
AMITKESARI2000 <kesari.amit33@gmail.com>
2021-08-31 10:10:35 +0530

    events card data adding

--- Histainability/app/src/main/java/com/dacarpas/histainability/GraphModel.kt
+++ Histainability/app/src/main/java/com/dacarpas/histainability/GraphModel.kt
@@ -216,27 +216,143 @@ object Supplier2 {
     // anand (9-12)
     val eventsGoal9 = listOf<EventData>(
         EventData(
+            1920,
+            "Great Industrial production in america",
+            "Mass production made technology affordable to the middle class. The automotive industry, the film industry, the radio industry, and the chemical industry took off during the 1920s.",
+            1
+        )
+        ,
+        EventData(
+            1930,
+            "Great Depression",
+            "While the October 1929 stock market crash triggered the Great Depression, multiple factors turned it into a decade-long economic catastrophe. Overproduction, executive inaction, ill-timed tariffs, and an inexperienced Federal Reserve all contributed to the Great Depression.",
+            1
+        ),
+        EventData(
+            1944,
+            "Military production in US",
+            "The demands of participating in the second World War revitalized American industry and made the United States a global leader in production. The enhanced warships and aircrafts provided by the United States were crucial to securing victory for the Allied forces.",
+            1
+        ),
+        EventData(
+            1960,
+            "Innovations",
+            "The rapidly decolonizing world began to collaborate more than ever, bringing in a new age of innovation in computers, household appliances, the emerging aerospace industry, entertainment, and in the medical fields.",
+            1
+        ),
+        EventData(
+            2001,
+            "Wikipedia",
+            "In January 2001, Wikipedia, the free user-generated online encyclopedia, came online and quickly became the reference site of choice for Internet users.",
+            1
+        ),
+        EventData(
+            2004,
+            "Google goes public",
+            "Larry Page and Sergey Brin incorporated Google in 1998, and in 2004 the company sold shares to the public for the first time, solidifying search as a way of life.",
+            1
+        ),
+        EventData(
+            2014,
+            "Make in India",
+            "Make in India is a major national programme of the Government of India designed to facilitate investment, foster innovation, enhance skill development, protect intellectual property and build best in class manufacturing infrastructure in the country.",
+            1
+        ),
+        EventData(
             2016,
-            "PMUY",
-            "PMUY was launched by the Government of India with the aim of providing clean-cooking fuel to the poor households, which are otherwise vulnerable to various health hazards associated with indoor air pollution and bringing in qualitative charges in the living standards.",
+            "Standup India",
+            "Stand-Up India was launched by Government of India on 5 April 2016 to support entrepreneurship among women and SC & ST communities. It is similar to but distinct from Startup India.",
+            1
+        ),
+        EventData(
+            2017,
+            "Startup India",
+            "Startup India is a flagship initiative of the Government of India, intended to catalyse startup culture and build a strong and inclusive ecosystem for innovation and entrepreneurship in India.",
             1
         )
     )
 
     val eventsGoal10 = listOf<EventData>(
         EventData(
-            2016,
-            "PMUY",
-            "PMUY was launched by the Government of India with the aim of providing clean-cooking fuel to the poor households, which are otherwise vulnerable to various health hazards associated with indoor air pollution and bringing in qualitative charges in the living standards.",
+            1830,
+            "Abolitionist Movement",
+            "The abolitionist movement was an organized effort to end the practice of slavery in the United States. The first leaders of the campaign, which took place from about 1830 to 1870, mimicked some of the same tactics British abolitionists had used to end slavery in Great Britain in the 1830s.",
             1
-        )
+        ),
+        EventData(
+            1948,
+            "Minimum wage Act",
+            "An Act to provide for fixing minimum rates of wages in certain employments.",
+            1
+        ),
+        EventData(
+            1950,
+            "Civil Rights Movement",
+            "The civil rights movement was a struggle for justice and equality for African Americans that took place mainly in the 1950s and 1960s. It was led by people like Martin Luther King Jr., Malcolm X, the Little Rock Nine and many others.",
+            1
+        ),
+        EventData(
+            1951,
+            "Land Reforms",
+            "The first and longest phase (1950 - 72) consisted of land reforms that included three major efforts: abolition of the intermediaries, tenancy reform, and the redistribution of land using land ceilings. The abolition of intermediaries was relatively successful, but tenancy reform and land ceilings met with less success.",
+            1
+        ),
+        EventData(
+            1970,
+            "Black History",
+            "Black History Month was created to focus attention on the contributions of African Americans to the United States. It honors all Black people from all periods of U.S. history, from the enslaved people first brought over from Africa in the early 17th century to African Americans living in the United States today.",
+            1
+        ),
+        EventData(
+            1979,
+            "Mandal Commission",
+            "The Mandal Commission, the Socially and Educationally Backward Classes Commission (SEBC), was established in India on 1979 by the Janata Party government under Prime Minister Morarji Desai with a mandate to "identify the socially or educationally backward classes" of India.",
+            1
+        ),
+        EventData(
+            2001,
+            "Gay Marraige",
+            "Same-sex marriage, also known as gay marriage or homosexual marriage, is the marriage of two people of the same sex or gender, entered into in a civil or religious ceremony. There are records of same-sex marriage dating back to the first century. In the modern era, marriage equality was first granted to same-sex couples in the Netherlands on 1 April 2001.",
+            1
+        ),
+        EventData(
+            2019,
+            "EWS Reservation",
+            "In 2019 the government announces the 10% reservation in educational institutions and government jobs for economically weaker section of the general category.",
+            1
+        ),
+        EventData(
+            2021,
+            "Transgender Reservation India",
+            "On 21 July 2021, Karnataka became the first state in the country to provide one percent reservation for the transgender community in all government services.",
+            1
+        ),
+
     )
   
     val eventsGoal11 = listOf<EventData>(
         EventData(
-            2016,
-            "PMUY",
-            "PMUY was launched by the Government of India with the aim of providing clean-cooking fuel to the poor households, which are otherwise vulnerable to various health hazards associated with indoor air pollution and bringing in qualitative charges in the living standards.",
+            1984,
+            "Metro India",
+            "The Kolkata Metro is the first planned and operational rapid transit system in India. It was initially planned in the 1920s, but construction started in the 1970s. The first underground stretch, from Bhawanipore (now Netaji Bhawan) to Esplanade, opened in 1984. Line 2, or the East–West Corridor, opened in 2020.",
+            1
+        ),
+        EventData(
+            2013,
+            "Solar Mission",
+            "The Rooftop PV and Small Scale Generation Programme (RPSSGP) aims to encourage the development of rooftop and ground-mounted solar systems. The Indian government revised the Solar Mission in 2014. It targets for 100 GW installed capacity of solar electricity by 2022.",
+            1
+        ),
+        EventData(
+            2014,
+            "Swachh Bharat Abhiyan",
+            "Swachh Bharat Mission, Swachh Bharat Abhiyan, or Clean India Mission is a country-wide campaign initiated by the Government of India in 2014 to eliminate open defecation and improve solid waste management.",
+            1
+        ),
+        EventData(
+            2015,
+            "Smart City India",
+            "National Smart Cities Mission is an urban renewal and retrofitting program by the Government of India with the mission to develop smart cities across the country, making them citizen friendly and sustainable.",
             1
         )
     )
@@ -244,36 +360,140 @@ object Supplier2 {
     val eventsGoal12 = listOf<EventData>(
         EventData(
             2016,
-            "PMUY",
-            "PMUY was launched by the Government of India with the aim of providing clean-cooking fuel to the poor households, which are otherwise vulnerable to various health hazards associated with indoor air pollution and bringing in qualitative charges in the living standards.",
+            "Reduce the emissions intensity",
+            "India made a commitment to reduce the emissions intensity of its GDP by 20-25% from its 2005 levels by 2020 and by 33-35% by 2030.",
+            1
+        ),
+        EventData(
+            2015,
+            "Paris Agreeement",
+            "The Paris Agreement, often referred to as the Paris Accords or the Paris Climate Accords, is an international treaty on climate change, adopted in 2015. It covers climate change mitigation, adaptation, and finance.",
             1
         )
     )
 
-    // sowmya
+    // sowmya (13-17)
     val eventsGoal13 = listOf<EventData>(
         EventData(
-            2016,
-            "PMUY",
-            "PMUY was launched by the Government of India with the aim of providing clean-cooking fuel to the poor households, which are otherwise vulnerable to various health hazards associated with indoor air pollution and bringing in qualitative charges in the living standards.",
+            1820,
+            "Greenhouse effect",
+            "Joseph Fourier proposed that energy reaching the planet as sunlight must be balanced by energy returning to space since heated surfaces emit radiation.",
+            1
+        ),
+        EventData(
+            1895,
+            "Regarding decreasing level of CO2",
+            "Swedish chemist Svante Arrhenius calculations showed that if CO2 levels were halved, global temperatures could decrease by about 5 degrees Celsius.",
+            1
+        ),
+        EventData(
+            1958,
+            "Monitoring station",
+            "Monitoring station established by the Scripps Institution of Oceanography on top of Hawaii’s Mauna Loa Observatory.",
+            1
+        ),
+        EventData(
+            1989,
+            "IPCC was established",
+            "Intergovernmental Panel on Climate Change (IPCC) was established under the United Nations to provide a scientific view of climate change and its political and economic impacts.",
+            1
+        ),
+        EventData(
+            1997,
+            "Kyoto Protocol",
+            "The first global agreement to reduce greenhouse gases, the Kyoto Protocol, was adopted in 1997.",
+            1
+        ),
+        EventData(
+            2006,
+            "Nobel Peace Prize",
+            "Former Vice President and presidential candidate Al Gore weighed in on the dangers of global warming with the debut of his film An Inconvenient Truth. Gore won the 2007 Nobel Peace Prize for his work on behalf of climate change.",
+            1
+        ),
+        EventData(
+            2018,
+            "Intergovernamental Panel on Climate Change",
+            "The United Nations body for assessing the science related to climate change, published a special report "Global Warming of 1.5°C"",
+            1
+        ),
+        EventData(
+            2015,
+            "Paris Agreement",
+            "Built by -  the United nations Framework Convention on Climate Change(UNFCCC)  at the 2015 united nations climate chnage conference in Paris by the parties.",
+            1
+        ),
+        EventData(
+            2018,
+            "School strike for climate ",
+            "Swedish teenager and climate activist Greta Thunberg began protesting in front of Swedish Parliament with a sign: 'School Strike for Climate'.",
+            1
+        ),
+        EventData(
+            2019,
+            "IPCC releases a refinement",
+            "the IPCC released the 2019 Refinement to the 2006 IPCC Guidelines on National Greenhouse Gas Inventories.",
             1
         )
+
     )
 
     val eventsGoal14 = listOf<EventData>(
         EventData(
+            1982,
+            "Exclusive economic zone",
+            "An exclusive economic zone (EEZ) is a sea zone prescribed by the 1982 United Nations Convention on the Law of the Sea over which a sovereign state has special rights regarding the exploration and use of marine resources, including energy production from water and wind.",
+            1
+        ),
+        EventData(
+            2015,
+            "Established Under Water constituion",
+            "Conserve and sustainably use the oceans, seas and marine resources for sustainable development",
+            1
+        ),
+        EventData(
+            2017,
+            "UN Conference for under water life",
+            "Sweden and Fiji co-hosted a high-level United Nations Conference to support the implementation of Sustainable Development Goal 14: Conserve and sustainably use the oceans, seas and marine resources for sustainable development.",
+            1
+        ),
+        EventData(
+            2008,
+            "EU Marine Strategy framework Directive",
+            "The Directive has three main goals: Europe’s seas should be (1) healthy, (2) clean and (3) productive.",
+            1
+        ),
+        EventData(
             2016,
             "PMUY",
             "PMUY was launched by the Government of India with the aim of providing clean-cooking fuel to the poor households, which are otherwise vulnerable to various health hazards associated with indoor air pollution and bringing in qualitative charges in the living standards.",
             1
         )
+        
     )
 
     val eventsGoal15 = listOf<EventData>(
         EventData(
             2016,
-            "PMUY",
-            "PMUY was launched by the Government of India with the aim of providing clean-cooking fuel to the poor households, which are otherwise vulnerable to various health hazards associated with indoor air pollution and bringing in qualitative charges in the living standards.",
+            "IUCN World Conservation Congress",
+            "The Secretariat of the UNCCD, the Economics of Land Degradation initiative, the IUCN Commission on Ecosystem Management and the IUCN Global Drylands initiative collaborated in organizing a themed journey entitled “Life on Land” at the IUCN World Conservation Congress, Hawaii, September 2016. The journey was designed to support a move towards a more comprehensive response to achieving Land Degradation Neutrality by 2030, as part of the Sustainable Development Goals. It connected many diverse approaches to land use and land management to explore potential synergies or conflicts",
+            1
+        ),
+         EventData(
+            1994,
+            "United Nation Convention to Combat Desertification (UNCCD)",
+            "India became a signatory to the United Nation Convention to Combat Desertification (UNCCD) in 1994 and ratified it in 1996.",
+            1
+        ),
+        EventData(
+            2002,
+            "World Summit on Sustainable Development",
+            "At the World Summit on Sustainable Development, held in Johannesburg 2002, biological diversity was addressed in Chapter IV, paragraph 44, of the outcome of the Summit, the Johannesburg Plan of Implementation. The Summit also endorsed the target to achieve, by 2010, a significant reduction of the rate of biodiversity loss at global, regional and national levels as a contribution to poverty alleviation and to the benefit of all life on earth, which had some months earlier been adopted by the sixth meeting of the CBD Conference of Parties (COP).",
+            1
+        ),
+        EventData(
+            1992,
+            "United Nations Conference on Environment and Development",
+            "Conservation of biological diversity is the subject of Chapter 15 of Agenda 21 which was adopted at the United Nations Conference on Environment and Development, in 1992, in Rio de Janeiro.",
             1
         )
     )
@@ -292,6 +512,25 @@ object Supplier2 {
             2015,
             "Trade- Average agrcultural tariffs",
             "The weighted average of applied tariffs has been decreasing over time, but still varies widely across regions and country groups on main product sectors. Average agricultural tariffs on imports coming into Asia, for instance, are the highest in the world, at around 27 per cent for Southern Asia and 20 per cent for Eastern Asia in 2015, under most-favoured nation status.",
+            1
+        ),
+        EventData(
+            2016,
+            "Birth Registrations",
+            "146 countries and territories have birth registration data that are at least 90 per cent complete and 151 countries and territories have death registration data that are at least 75 per cent complete. Therefore, about 60 per cent of countries and territories worldwide meet both criteria. In sub-Saharan Africa, additional countries and territories have birth and death registration data compared to the previous reporting period, increasing from 28 to 29 countries and territories for birth registration and from 53 to 55 countries and territories for death registration.",
+            1
+        )
+        ,
+        EventData(
+            2019,
+            "National statistical legislation",
+            "122 Member States reported having national statistical legislation that was compliant with the Fundamental Principles of Official Statistics. The proportions of compliant Member States exceed 40 per cent for almost all regions and country groups, except for the least developed countries.",
+            1
+        ),
+        EventData(
+            2020,
+            "National statistical plan",
+            "132 countries and territories reported implementing a national statistical plan, with 84 having fully funded plans. Only 4 out of 46 (8 per cent) of the least developed countries reported having fully funded national statistical plans.",
             1
         )
     )
